371580b21fc1d9e20d7bd22bcc339cac851b03b3
Shaima Hamila <hamilachaima18@gmail.com>
2023-11-26 20:07:32 +0100

    create club feature

--- src/main/java/com/dev/examplespringboot/controllers/ClubController.java
+++ src/main/java/com/dev/examplespringboot/controllers/ClubController.java
@@ -1,12 +1,15 @@
 package com.dev.examplespringboot.controllers;
 
 import com.dev.examplespringboot.dto.ClubDto;
+import com.dev.examplespringboot.models.Club;
 import com.dev.examplespringboot.services.ClubService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +27,18 @@ public class ClubController {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model){
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club){
+        clubService.saveClub(club);
+        return "redirect:/clubs";
     }
 }
